006b823c49a0efb9b49f78d3d47a1f7cbdb30ced
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2025-09-29 01:24:05 +0530

    profile page posts and User info fetched!

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -19,7 +19,7 @@ router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
-router.post("/likes/:id", protectRoute, getLikedPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
 router.get("/user/:username", protectRoute, getUserPosts);
 
 export default router;
